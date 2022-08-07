e3255c485e65e54dc742f3a4e644fd2537ac9340
xsaints <xsaints@yahoo.com>
2022-08-07 06:28:11 -0600

    added static folders

--- myproj01/urls.py
+++ myproj01/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+from . import views
+
+
 urlpatterns = [
     path('app01/', include('app01.urls')),
+    
+    path('', views.home, name= 'home'),
     
     path('admin/', admin.site.urls),
 ]
